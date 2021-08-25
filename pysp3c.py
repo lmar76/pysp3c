984c264555b14530d9273cde019825235235af94
Luca Mariani <lmar76@gmail.com>
2021-08-25 18:57:50 +0200

    Updated SP3cReader

--- pysp3c.py
+++ pysp3c.py
@@ -1,7 +1,7 @@
 """SP3c file reader."""
 import os
 import pathlib
-from typing import Union
+from typing import Dict, List, Union
 
 __version__ = '0.1'
 
@@ -16,7 +16,58 @@ class SP3cReader:
 
     def __init__(self, file: Union[str, os.PathLike]):
         self._file = pathlib.Path(file).expanduser()
+        with self._file.open() as fh:
+            record = ''
+            data = []
+            for n, text in enumerate(fh, 1):
+                if n > 22:
+                    if text.startswith('* '):
+                        if record != '':
+                            # process record
+                            data.extend(self._parse_record(record))
+                            # reset record
+                            record = ''
+                        record += text
+                    elif text.startswith('EOF'):
+                        # last line: process record
+                        data.extend(self._parse_record(record))
+                    else:
+                        record += text
+            self._data = data
+
+    @staticmethod
+    def _parse_record(record: str) -> List[Dict[str, Union[int, float, str]]]:
+        lines = record.strip().split('\n')
+        result = []
+        # parse epoch
+        year = int(lines[0][3:7])
+        month = int(lines[0][8:10])
+        day = int(lines[0][11:13])
+        hour = int(lines[0][14:16])
+        minute = int(lines[0][17:19])
+        second = float(lines[0][20:31])
+        for line in lines[1:]:
+            obs = {
+                'year': year,
+                'month': month,
+                'day': day,
+                'hour': hour,
+                'minute': minute,
+                'second': second,
+                'type': line[0],
+                'vehicle_id': line[1:4],
+                'x': float(line[4:18]),
+                'y': float(line[18:32]),
+                'z': float(line[32:46]),
+                'clock': float(line[46:60]),
+            }
+            result.append(obs)
+        return result
 
     @property
     def file(self):
         return self._file
+
+    @property
+    def data(self):
+        return self._data
--- tests/test_pysp3c.py
+++ tests/test_pysp3c.py
@@ -23,3 +23,33 @@ class TestSP3cReader:
                 pysp3c.SP3cReader(shared_datadir / file),
                 pysp3c.SP3cReader
             )
+
+    @pytest.mark.parametrize(
+        'file, expected',
+        [
+            (
+                'test.sp3',
+                [
+                    {
+                        'year': 2021,
+                        'month': 8,
+                        'day': 11,
+                        'hour': 0,
+                        'minute': 0,
+                        'second': 18.0,
+                        'type': 'P',
+                        'vehicle_id': 'L47',
+                        'x': 6461.886846,
+                        'y': 2100.852059,
+                        'z': -473.263449,
+                        'clock': 0.007519,
+                    }
+                ]
+            )
+        ]
+    )
+    def test_data(self, shared_datadir, file, expected):
+        """Test the `SP3cReader.data` property."""
+        sp3 = pysp3c.SP3cReader(shared_datadir / file)
+        assert len(sp3.data) == 8
+        assert sp3.data[0] == expected[0]
